0ab574557052575fb20c5cc03343a9eb2273a928
sbx_user1059 <sbx_user1059@ip-10-128-206-243.eu-west-2.compute.internal>
2018-10-16 05:39:05 +0000

    Tue Oct 16 05:39:05 UTC 2018

--- src/test/java/befaster/solutions/CHK/CHKTest.java
+++ src/test/java/befaster/solutions/CHK/CHKTest.java
@@ -1,6 +1,37 @@
 package befaster.solutions.CHK;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class CHKTest {
+	
+	
+	private CheckoutSolution checkSol;
+	
+	@Before
+	public void setUp() {
+		checkSol = new CheckoutSolution();
+	}
+	
+	@Test
+	public void computeCase1(){
+		
+		assertEquals((Integer)660, checkSol.checkout("6A,10B,5C,5D"));
+	}
+	
+	@Test
+	public void computeCase2(){
+		
+		assertEquals((Integer)50, checkSol.checkout("1A"));
+	}
+	
+	@Test
+	public void computeCase3(){
+		
+		assertEquals((Integer)15000, checkSol.checkout("1000D"));
+	}
 
 }
 
