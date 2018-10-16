ad333bb73a92d215db358a7af403a1ccf1b999b4
sbx_user1059 <sbx_user1059@ip-10-128-206-243.eu-west-2.compute.internal>
2018-10-16 05:12:05 +0000

    Tue Oct 16 05:12:05 UTC 2018

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,55 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.HashMap;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+        
+    	int totalPrice = 0;
+    	HashMap<String, Integer> hmap = new HashMap<String, Integer>();
+    	
+    	String[] products = skus.split(",");
+    	
+    	for(String product : products) {
+    		
+    			processProductString(hmap, product);
+    		
+    	}
+    	
+    	
+    	//throw new SolutionNotImplementedException();
+    }
+    
+    private int specialOffer(String item, int quantity) {
+    	int totalPrice = 0;
+    	
+    	if(item.equals("A")) {
+    		
+    		totalPrice = (quantity / 3) * 130 + (quantity % 3) * 50;
+    		
+    	}
+    	
+    	else if(item.equals("B")) {
+    		totalPrice = (quantity / 2) * 45 + (quantity % 2) * 30;
+    	}
+    	
+    	else if(item.equals("C")) {
+    		totalPrice = quantity * 20;
+    	}
+    	else if(item.equals("D")) {
+    		totalPrice = quantity * 15;
+    	}
+    	
+    	return totalPrice;
+    	
+    }
+    
+    
+    private void processProductString(HashMap<String, Integer> map, String proString) {
+    	
+    	String quantityString = proString.substring(0, proString.length() - 2);
+    	int quantity = Integer.parseInt(quantityString);
+    	map.put(proString.charAt(proString.length() - 1) + "", quantity);
+    	    	
     }
 }
